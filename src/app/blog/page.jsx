9373acfcb436144f726b733ceb1863de95359560
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-08 20:30:52 -0300

    feat: add initial Blog page

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,16 @@
+import CardList from "@/components/cardList/CardList";
+import styles from "./blogPage.module.css";
+import Menu from "@/components/menu/Menu";
+
+const BlogPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Blog de Estilos</h1>
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
+};
+export default BlogPage;
