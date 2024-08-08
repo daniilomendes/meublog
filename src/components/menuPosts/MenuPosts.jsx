47cf7b08691e74035d23e4820cdd13df89086f80
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-08 20:06:07 -0300

    feat: add init MenuPosts component

---
+++ src/components/menuPosts/MenuPosts.jsx
@@ -0,0 +1,88 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./menuPosts.module.css";
+
+const MenuPosts = ({ withImage }) => {
+  return (
+    <div className={styles.items}>
+      <Link href="/" className={styles.item}>
+        {withImage && (
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+        )}
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.travel}`}>Viagens</span>
+          <h3 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Danilo Mendes</span>
+            <span className={styles.date}> - 10.03.2024</span>
+          </div>
+        </div>
+      </Link>
+
+      <Link href="/" className={styles.item}>
+        {withImage && (
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+        )}
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.culture}`}>
+            Cultura
+          </span>
+          <h3 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Danilo Mendes</span>
+            <span className={styles.date}> - 10.03.2024</span>
+          </div>
+        </div>
+      </Link>
+
+      <Link href="/" className={styles.item}>
+        {withImage && (
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+        )}
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.coding}`}>
+            Programação
+          </span>
+          <h3 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Danilo Mendes</span>
+            <span className={styles.date}> - 10.03.2024</span>
+          </div>
+        </div>
+      </Link>
+
+      <Link href="/" className={styles.item}>
+        {withImage && (
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+        )}
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.style}`}>Estilo</span>
+          <h3 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Danilo Mendes</span>
+            <span className={styles.date}> - 10.03.2024</span>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default MenuPosts;
