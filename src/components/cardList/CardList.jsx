b9c7f67e34ac09421eec2b2379fd21157777e808
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-08 11:13:38 -0300

    feat: add init CardList component

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,7 +1,20 @@
+import Pagination from "../pagination/Pagination";
 import styles from "./cardList.module.css";
+import Card from "../card/Card";
 
 const CardList = () => {
-  return <div className={styles.container}>CardList</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Posts Recentes</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Pagination />
+    </div>
+  );
 };
 
 export default CardList;
