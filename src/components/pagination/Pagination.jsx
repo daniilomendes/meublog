4f80074bc42f2d2992354fcc6405603e2643a8d9
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-08 20:08:40 -0300

    feat: add init Pagination component

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,7 +1,12 @@
 import styles from "./pagination.module.css";
 
 const Pagination = () => {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>Anterior</button>
+      <button className={styles.button}>Próximo</button>
+    </div>
+  );
 };
 
 export default Pagination;
