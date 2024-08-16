d05a0bdd7308ad87a794505c7a99eb0b745957aa
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-16 15:12:07 -0300

    feat: add traduction and dinamic types in blogs page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,12 +2,23 @@ import CardList from "@/components/cardList/CardList";
 import styles from "./blogPage.module.css";
 import Menu from "@/components/menu/Menu";
 
-const BlogPage = () => {
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
+  let traducao;
+  if (cat === "fashion") traducao = "Modas";
+  if (cat === "culture") traducao = "Culturas";
+  if (cat === "coding") traducao = "Códigos";
+  if (cat === "food") traducao = "Comidas";
+  if (cat === "style") traducao = "Estilos";
+  if (cat === "travel") traducao = "Viagens";
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Blog de Estilos</h1>
+      <h1 className={styles.title}>{traducao}</h1>
       <div className={styles.content}>
-        <CardList />
+        <CardList page={page} cat={cat} />
         <Menu />
       </div>
     </div>
