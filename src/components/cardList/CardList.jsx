1c5a99835008a71b397f52a929a534e78aa6eccc
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-16 14:48:21 -0300

    feat: add dinamic information and pagination

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,17 +2,35 @@ import Pagination from "../pagination/Pagination";
 import styles from "./cardList.module.css";
 import Card from "../card/Card";
 
-const CardList = () => {
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CardList = async ({ page }) => {
+  const { posts, count } = await getData(page);
+
+  const POST_PER_PAGE = 2;
+
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Posts Recentes</h1>
       <div className={styles.posts}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {posts?.map((item) => (
+          <Card key={item._id} item={item} />
+        ))}
       </div>
-      <Pagination />
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
 };
