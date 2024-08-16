b115a3133495c261eda6ea11a78330dc6f06fa89
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-16 17:08:07 -0300

    feat: ensuring that the user can create comments

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -5,6 +5,7 @@ import styles from "./comments.module.css";
 import Image from "next/image";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -22,10 +23,21 @@ const fetcher = async (url) => {
 const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
-  const { data, isLoading } = useSWR(
+  const { data, mutate, isLoading } = useSWR(
     `http://localhost:3000/api/comments?postSlug=${postSlug}`,
     fetcher
   );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+
+    mutate();
+  };
 
   return (
     <div className={styles.container}>
@@ -36,8 +48,11 @@ const Comments = ({ postSlug }) => {
           <textarea
             placeholder="Escreva um comentário..."
             className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
           />
-          <button className={styles.button}>Enviar</button>
+          <button className={styles.button} onClick={handleSubmit}>
+            Enviar
+          </button>
         </div>
       ) : (
         <Link href="/login">Faça login para escrever um comentário.</Link>
