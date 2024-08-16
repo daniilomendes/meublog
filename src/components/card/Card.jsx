e541bdc3f75b9731db36c5b6abebffc7766c6640
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-16 15:19:57 -0300

    refact: add dinamic props

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,26 +2,42 @@ import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
+  let traducao;
+  if (item.catSlug === "fashion") traducao = "Moda";
+  if (item.catSlug === "culture") traducao = "Cultura";
+  if (item.catSlug === "coding") traducao = "Código";
+  if (item.catSlug === "food") traducao = "Comida";
+  if (item.catSlug === "style") traducao = "Estilo";
+  if (item.catSlug === "travel") traducao = "Viagen";
+
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
+    <div className={styles.container} key={key}>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image
+            src={item.img}
+            alt={item.title}
+            fill
+            className={styles.image}
+          />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{traducao}</span>
         </div>
-        <Link href="/">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A tempore
-          tenetur quasi sunt pariatur hic velit qui cumque architecto ullam
-          veniam, voluptate omnis. Velit quos perspiciatis ex.
-        </p>
-        <Link href="/" className={styles.link}>
+        <div
+          className={styles.desc}
+          dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 60) }}
+        />
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
           Leia mais
         </Link>
       </div>
