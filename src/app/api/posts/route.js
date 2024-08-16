303b488704a6eb1c6aae8a7552664a859b23eefa
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-16 18:21:47 -0300

    feat: add post rout

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
@@ -24,6 +25,33 @@ export const GET = async (req) => {
     ]);
 
     return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Algo deu errado!" }, { status: 500 })
+    );
+  }
+};
+
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Não Autenticado!" }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const post = await prisma.post.create({
+      data: {
+        ...body,
+        userEmail: session.user.email,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(post, { status: 200 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
