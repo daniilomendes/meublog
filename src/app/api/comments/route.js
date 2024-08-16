6b987453d23e236aef076e65fa1f446cb7cefbb2
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-16 17:07:05 -0300

    feat: add commets api route

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
@@ -14,6 +15,33 @@ export const GET = async (req) => {
     });
 
     return new NextResponse(JSON.stringify(comments, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Algo deu errado!" }, { status: 500 })
+    );
+  }
+};
+
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Não Autenticado!" }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: {
+        ...body,
+        userEmail: session.user.email,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
